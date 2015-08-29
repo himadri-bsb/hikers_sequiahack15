a869fde7c06290a7b9704da346ece65cc4bc01ed
Ravindra Shetty <ravindra@hike.in>
2015-08-29 16:50:24 +0530

    Added left menu

---
+++ Client/hikerapp/hikerapp/HALeftMenuTableViewController.m
@@ -0,0 +1,72 @@
+//
+//  HALeftMenuTableViewController.m
+//  hikerapp
+//
+//  Created by Ravindra Shetty on 29/08/15.
+//  Copyright (c) 2015 Hike. All rights reserved.
+//
+
+#import "HALeftMenuTableViewController.h"
+#import "UIViewController+RESideMenu.h"
+#import "RESideMenu.h"
+
+@interface HALeftMenuTableViewController ()
+@property (nonatomic, strong) NSArray *menuItems;
+@end
+
+@implementation HALeftMenuTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.menuItems = @[@"Home", @"Settings", @"LogOut"];
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    self.tableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.menuItems count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"menuCell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"menuCell"];
+        cell.backgroundColor = [UIColor clearColor];
+        cell.textLabel.textColor = [UIColor whiteColor];
+    }
+    cell.textLabel.text = [self.menuItems objectAtIndex:indexPath.row];
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+// In a xib-based application, navigation from a table can be handled in -tableView:didSelectRowAtIndexPath:
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        
+    }
+    else if(indexPath.row == 1) {
+        
+    }
+    else {
+        
+    }
+}
+
+@end
