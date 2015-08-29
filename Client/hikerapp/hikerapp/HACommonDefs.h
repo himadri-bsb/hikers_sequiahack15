56885cc823d584006332eb0ae790b5087fd03e72
Himadri Jyoti <himadri@bsb.in>
2015-08-29 17:36:24 +0530

    Add model file

---
+++ Client/hikerapp/hikerapp/HACommonDefs.h
@@ -0,0 +1,13 @@
+//
+//  HACommonDefs.h
+//  hikerapp
+//
+//  Created by Himadri Jyoti on 29/08/15.
+//  Copyright (c) 2015 Hike. All rights reserved.
+//
+
+#ifndef hikerapp_HACommonDefs_h
+#define hikerapp_HACommonDefs_h
+
+
+#endif
